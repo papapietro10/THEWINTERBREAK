7d5bb6fbe0b34f537ab4f065ad2657d7fcb5294d
Game-store <duboy36@gmail.com>
2023-01-02 19:34:01 -0300

    cards

--- src/components/contenedor/ProductoDetalles/ProductoDetalles.js
+++ src/components/contenedor/ProductoDetalles/ProductoDetalles.js
@@ -2,31 +2,37 @@ import './ProductoDetalles.css';
 import {Link} from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+
+import React, {useState} from 'https://cdn.skypack.dev/react';
+import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+import {TiChevronLeftOutline, TiChevronRightOutline} from 'https://cdn.skypack.dev/react-icons/ti';
 
 export const Item = ({item})=>{
     return(
-        <div class="container">
+      <figure className="snip1195">
+      <h4>{item.TiChevronRightOutline}</h4>
+      <div className="image">
+        <img src={item.pictureUrl} alt="" />
+      </div>
+      <div className="rating">
+     
+          <i className="ion-ios-star" />
+          <i className="ion-ios-star" />
+          <i className="ion-ios-star-outline" />
+          <i className="ion-ios-star-outline" />
         
-        <div class="card float-left">
-          <div class="row ">
-      
-            <div class="col-sm-7">
-              <div class="card-block">
-            
-                <p>{item.title}</p>
-                <p>Change around the content for awsomenes</p>
-              <Button > Detalle</Button>
-              </div>
-            </div>
-      
-            <div class="col-sm-5">
-            <img className="d-block" src="" alt="" />
-            </div>
-          </div>
+      </div>
+      <figcaption>
+        <p>dgdgfgdgdgdf</p>
+        <div className="price">
+          <s>$</s>$
         </div>
-      
-       
-        </div>
+      </figcaption>
+      <a href="#" className="add-to-cart">
+        Add to Cart
+      </a>
+    </figure>
        
      
     )
--- src/components/contenedor/inicio-uno/InicioUno.js
+++ src/components/contenedor/inicio-uno/InicioUno.js
@@ -13,10 +13,10 @@ export const InicioUno = ()=>{
 <div>
        <div className='container-inicio-uno'>
        <div className='inicio-titulo'>
-            <h4 data-aos="fade-right" data-aos-duration="800">Snowboard</h4>
-            <h4 data-aos="fade-right" data-aos-duration="800">Amigos</h4>
-            <h4 data-aos="fade-right" data-aos-duration="800">Fiesta</h4>
-            <h4 data-aos="fade-right" data-aos-duration="800">Ski</h4>
+            <h4 data-aos="fade-right" data-aos-duration="1000">Snowboard</h4>
+            <h4 data-aos="fade-right" data-aos-duration="1000">Amigos</h4>
+            <h4 data-aos="fade-right" data-aos-duration="1000">Fiesta</h4>
+            <h4 data-aos="fade-right" data-aos-duration="1000">Ski</h4>
             </div>
             <div className='inicio-imagen'>
             <img data-aos="fade-right" data-aos-duration="800" src="https://firebasestorage.googleapis.com/v0/b/thewinterbreack.appspot.com/o/pngwing.com%20(1).png?alt=media&token=aff9f31c-de2b-4cb5-af22-1f9cb850a636" alt=""  />
